de72cb0e56c6ccb95355de082201d7be76c6b9d5
teyim <teyimasobo@gmail.com>
2021-08-06 17:08:46 +0200

    create calendar component

---
+++ src/Components/Calender/Calender.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Calender.css";
+import DatePicker from "react-datepicker";
+
+export default function Calender() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(
+    new Date().setMonth(startDate.getMonth() + 1),
+  );
+
+  return (
+    <div className="bg-gray-100">
+      <div className="flex items-center max-w-2xl mx-auto justify-center space-x-4">
+        <div>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            minDate={new Date()}
+            maxDate={endDate}
+            nextMonthButtonLabel=">"
+            previousMonthButtonLabel="<"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
