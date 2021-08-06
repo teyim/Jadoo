fe9205e10a070d87a6b6d643665acbc41b611cd5
teyim <teyimasobo@gmail.com>
2021-08-06 17:07:21 +0200

    setup project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
+// tailwind.config.js
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        indigo: colors.indigo,
+        pink: colors.pink,
+      },
+    },
+    fontFamily: {
+      boing: ["GD Boing", "cursive"],
+      comfortaa: ["Comfortaa", "cursive"],
+      rubik: ["Rubik", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
