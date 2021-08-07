a4ce8320cf0f1b6256b07442ac4b2b71dd6cbb5f
teyim <teyimasobo@gmail.com>
2021-08-07 02:50:12 +0200

    modify header section

--- src/Components/Layout/Header/Header.js
+++ src/Components/Layout/Header/Header.js
@@ -5,8 +5,17 @@ import Calender from "Components/Calender/Calender";
 import Button from "Components/Button/Button";
 
 export default function Header() {
+  const routes = [
+    { from: "Bamenda", to: "Douala" },
+    { from: "Bamenda", to: "Yaounde" },
+    { from: "Limbe", to: "Douala" },
+    { from: "Buea", to: "Maroua" },
+  ];
+
   return (
-    <div className=" max-w-8xl text-gray-700 font-rubik  bg-gradient-to-r from-blue-600 via-blue-700 to-blue-600 flex flex-col items-center overflow-hidden">
+    <div
+      className=" max-w-8xl py-4
+    text-gray-700 font-rubik  bg-gradient-to-r from-blue-600 via-blue-700 to-blue-600 flex flex-col items-center overflow-hidden">
       <h1 className="mt-3 pt-4 font-semibold text-4xl text-center text-white ">
         Bus Ticket booking made Easy
       </h1>
@@ -44,33 +53,17 @@ export default function Header() {
         <p className="py-1 px-2 rounded-3xl text-gray-700 border shadow bg-blue-300">
           Popular Bus routes
         </p>
-        <div className="flex px-3 ">
-          <p className="py-1 px-1 rounded shadow border text-gray-700  bg-blue-200">
-            Bamenda
-          </p>
-          <SwitchHorizontalIcon className="w-6 h-6 my-auto text-gray-50"></SwitchHorizontalIcon>
-          <p className="py-1 px-1 rounded shadow border text-gray-700  bg-blue-200">
-            Douala
-          </p>
-        </div>
-        <div className="flex px-3 ">
-          <p className="py-1 px-1 rounded shadow border text-gray-700  bg-blue-200">
-            Buea
-          </p>
-          <SwitchHorizontalIcon className="w-6 h-6 my-auto text-gray-50"></SwitchHorizontalIcon>
-          <p className="py-1 px-1 rounded shadow border text-gray-700  bg-blue-200">
-            Douala
-          </p>
-        </div>
-        <div className="flex px-3 ">
-          <p className="py-1 px-1 rounded shadow border text-gray-700  bg-blue-200">
-            Bamenda
-          </p>
-          <SwitchHorizontalIcon className="w-6 h-6 my-auto text-gray-50"></SwitchHorizontalIcon>
-          <p className="py-1 px-1 rounded shadow border text-gray-700  bg-blue-200">
-            Yaounde
-          </p>
-        </div>
+        {routes.map((route) => (
+          <div className="flex px-3 ">
+            <p className="py-1 px-1 rounded shadow border text-gray-700  bg-blue-200">
+              {route.from}
+            </p>
+            <SwitchHorizontalIcon className="w-6 h-6 my-auto text-gray-50"></SwitchHorizontalIcon>
+            <p className="py-1 px-1 rounded shadow border text-gray-700  bg-blue-200">
+              {route.to}
+            </p>
+          </div>
+        ))}
       </div>
       <div>
         <Button
