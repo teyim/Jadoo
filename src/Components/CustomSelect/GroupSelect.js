d55234c52c07fd3f42f1aa5220a660a1d25137eb
teyim <teyimasobo@gmail.com>
2021-10-01 15:41:25 +0200

    Modify GroupSelect component

--- src/Components/CustomSelect/GroupSelect.js
+++ src/Components/CustomSelect/GroupSelect.js
@@ -4,18 +4,27 @@ import makeAnimated from "react-select/animated";
 
 const animatedComponents = makeAnimated();
 
-export default function GroupSelect() {
+export default function GroupSelect({height}) {
   const options = [
     { value: "chocolate", label: "Chocolate" },
     { value: "strawberry", label: "Strawberry" },
     { value: "vanilla", label: "Vanilla" },
   ];
+  const inputStyle = () => ({
+    alignItems: 'center',
+    display: 'flex',
+    height: height,
+  });
+  const colourStyles = {
+    input: (styles) => ({ ...styles, ...inputStyle() }),
+  };
   return (
     <Select
       closeMenuOnSelect={false}
       components={animatedComponents}
       isMulti
       options={options}
+      styles={colourStyles}
     />
   );
 }
