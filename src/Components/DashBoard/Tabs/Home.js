2157c2ddd4c8c44633ba07827c063e32e2748e40
teyim <teyimasobo@gmail.com>
2021-12-05 00:28:21 +0200

    Modify dashboard home

--- src/Components/DashBoard/Tabs/Home.js
+++ src/Components/DashBoard/Tabs/Home.js
@@ -25,7 +25,7 @@ export default function Dashboard() {
 
     return (
         <div>
-            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto font-rubik">
 
                 {/* Welcome banner */}
                 <WelcomeBanner />
@@ -37,7 +37,7 @@ export default function Dashboard() {
                     ))}
                 </div>
                  <div className='my-8'>
-                     <Table></Table>
+                     <Table tableHeading='Recent Bookings'></Table>
                  </div>
             </div>
         </div>
