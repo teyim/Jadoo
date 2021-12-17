cd2f4edb51c983e2a67a126a4a7a6e00d682e1ae
teyim <teyimasobo@gmail.com>
2021-12-17 02:27:03 +0200

    fix calender style

--- src/Components/Calender/Calender.js
+++ src/Components/Calender/Calender.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Calender.css";
 import DatePicker from "react-datepicker";
 
-export default function Calender() {
+export default function Calender({padding}) {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(
     new Date().setMonth(startDate.getMonth() + 1),
@@ -10,10 +10,8 @@ export default function Calender() {
 
   return (
     <div className="bg-gray-100">
-      <div className="flex items-center max-w-2xl mx-auto justify-center space-x-4">
-        <div>
           <DatePicker
-            className="py-3 "
+            className={`${padding} w-full`}
             selected={startDate}
             onChange={(date) => setStartDate(date)}
             minDate={new Date()}
@@ -21,8 +19,6 @@ export default function Calender() {
             nextMonthButtonLabel=">"
             previousMonthButtonLabel="<"
           />
-        </div>
       </div>
-    </div>
   );
 }
