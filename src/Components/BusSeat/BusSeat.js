8c9ea20dbf8469e1159c3c8b586eb6fea39faf56
teyim <teyimasobo@gmail.com>
2022-01-04 12:45:41 +0200

    Modify busSeat component

--- src/Components/BusSeat/BusSeat.js
+++ src/Components/BusSeat/BusSeat.js
@@ -106,7 +106,7 @@ export default function BusSeat() {
   {selectedSeats?.length === 4 ? <h2 className="font-rubik text-md font-medium text-red-400 text-center mb-0"><AiOutlineWarning className="w-5 h-5 my-auto mb-1 inline"/>Cannot book more than 4 seats</h2>:null}
    <div className=" bg-white md:px-4 w-full py-8 md:flex ">
     <div className="md:w-3/4 w-full h-48 flex border rounded border-gray-300 ">
-     <div className="w-1/6 h-full flex flex-col justify-end p-5 ">
+     <div className="w-1/6 h-full md:flex flex-col p-4 justify-end hidden">
       <GiSteeringWheel className="w-8 h-8"></GiSteeringWheel>
      </div>
      <div className="w-full flex flex-col content-between pt-4">
@@ -215,7 +215,7 @@ export default function BusSeat() {
    </div>
 
    <div className=" w-1/2 mx-auto md:mx-0 md:px-6 md:flex md:w-1/3  py-2 md:justify-evenly">
-    <div>
+    <div className="my-2 md:my-0">
      {" "}
      <div className="border rounded bg-gray-300 text-center">
       <div className="w-4/5 mx-auto  font-medium text-center bg-gray-400 border-2 rounded cursor-pointer">
@@ -224,7 +224,7 @@ export default function BusSeat() {
      </div>
      <p>Available</p>
     </div>
-    <div>
+       <div className="my-2 md:my-0">
      {" "}
      <div className="border rounded bg-gray-500 text-center">
       <div className="w-4/5 font-medium mx-auto text-center bg-gray-500 border-2 rounded cursor-pointer">
@@ -233,7 +233,7 @@ export default function BusSeat() {
      </div>
      <p>Booked</p>
     </div>
-    <div>
+       <div className="my-2 md:my-0">
      {" "}
      <div className="border rounded bg-green-500 text-center">
       <div className="w-4/5 mx-auto text-center font-medium  bg-green-600 border-2 rounded cursor-pointer">
