661e8b5abee9fa68e029d52d798ac570889a6044
teyim <teyimasobo@gmail.com>
2022-01-08 17:06:30 +0200

    Modify App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React,{useEffect} from "react";
 import Home from "Views/Home";
+import UserDashboard from "Views/UserDashboard";
 import "./App.css";
+import ScrollToTop from "Components/ScrollToTop";
+import { useDispatch,useSelector } from 'react-redux';
+import {auth} from './config/firebaseConfig'
+import { initState,SET_INITIALISED } from './Features/userAuth'
 
 function App() {
+  const dispatch = useDispatch()
+  const { initialised,user,userData } = useSelector(state => state.user)
+  
+  useEffect(()=>{
+    const cleanUp=auth.onAuthStateChanged((user) => {
+      if (user && !initialised) {
+           dispatch(initState())
+        }
+      !initialised && dispatch(SET_INITIALISED(true))
+    })
+    return cleanUp
+  },[])
+  
   return (
     <>
+    <div>{user?.email}</div>
+      <div>{userData?.email}</div>
      <Home></Home>
     </>
   );
