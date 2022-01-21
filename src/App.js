4d594c261e3b9394cefd576f90f1e570fefff0a2
teyim <teyimasobo@gmail.com>
2022-01-21 16:19:51 +0200

    Modify App component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React,{useEffect} from "react";
 import Home from "Views/Home";
-import UserDashboard from "Views/UserDashboard";
-import "./App.css";
 import { useDispatch,useSelector } from 'react-redux';
 import {auth} from './config/firebaseConfig'
 import { initState,SET_INITIALISED } from './Features/userAuth'
+import LoadingScreen from "Views/LoadingScreen";
 
 function App() {
   const dispatch = useDispatch()
-  const { initialised,user,userData } = useSelector(state => state.user)
+  const { initialised,initialLoad} = useSelector(state => state.user)
+  
+
   
   useEffect(()=>{
     const cleanUp=auth.onAuthStateChanged((user) => {
       if (user && !initialised) {
            dispatch(initState())
         }
-      !initialised && dispatch(SET_INITIALISED(true))
+      if(!user && !initialised)
+      {
+        dispatch(SET_INITIALISED(true))
+      }
     })
+
     return cleanUp
   },[])
-  
+
+   
+
+
   return (
     <>
-     <Home></Home>
+      {(!initialised || initialLoad) ?<LoadingScreen/>:<Home/>}
     </>
   );
 }
